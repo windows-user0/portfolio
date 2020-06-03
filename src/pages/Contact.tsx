aecd0e771574a1a3d350626e3cd5effa50ccecff
windows-user0 <windows-user0@protonmail.com>
2020-06-03 20:24:47 +0200

    Added sucess message

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -18,6 +18,8 @@ export default () => {
     email: "",
     message: "",
   });
+
+  const [formSubmitted, setFormSubmitted] = React.useState(false);
   const [buttonDisabled, setButtonDisabled] = React.useState(false);
 
   function handleUpdate(event: any) {
@@ -31,65 +33,80 @@ export default () => {
       .database()
       .ref("ContactForm/" + Date.now())
       .set(form);
+    setFormSubmitted(true);
   }
 
   return (
     <section className=" body-font flex flex-col h-full ">
       <div className=" px-5 py-24 ">
-        <div className="flex flex-col text-center w-full mb-12">
-          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
-            Contact Me
-          </h1>
-
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            I will get back to you as soon as possible.
-          </p>
-        </div>
-
-        <div className="lg:w-1/2 md:w-2/3 mx-auto">
-          <div className="flex flex-wrap -m-2">
-            <div className="p-2 w-1/2">
-              <input
-                name="name"
-                value={form.name}
-                onChange={handleUpdate}
-                className="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
-                placeholder="Name"
-                type="text"
-              />
-            </div>
-            <div className="p-2 w-1/2">
-              <input
-                name="email"
-                value={form.email}
-                onChange={handleUpdate}
-                className="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
-                placeholder="Email"
-                type="email"
-              />
-            </div>
-            <div className="p-2 w-full">
-              <textarea
-                value={form.message}
-                name="message"
-                onChange={handleUpdate}
-                className="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none h-48 focus:border-indigo-500 text-base px-4 py-2 resize-none block"
-                placeholder="Message"
-              ></textarea>
-            </div>
-            <div className="p-2 w-full">
-              <button
-                onClick={handleSend}
-                disabled={buttonDisabled}
-                className={`${
-                  buttonDisabled && "opacity-50 cursor-not-allowed"
-                } flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg`}
-              >
-                Button
-              </button>
-            </div>
+        {formSubmitted ? (
+          <div className="flex flex-col">
+            <span className="text-4xl flex justify-center text-center">
+              Thank You!
+            </span>
+            <span className="text-3xl flex justify-center text-center">
+              Your message is on its way to me
+            </span>
+            <img className="mt-10" src="/success-hedgehog.png" />
           </div>
-        </div>
+        ) : (
+          <>
+            <div className="flex flex-col text-center w-full mb-12">
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
+                Contact Me
+              </h1>
+
+              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                I will get back to you as soon as possible.
+              </p>
+            </div>
+
+            <div className="lg:w-1/2 md:w-2/3 mx-auto">
+              <div className="flex flex-wrap -m-2">
+                <div className="p-2 w-1/2">
+                  <input
+                    name="name"
+                    value={form.name}
+                    onChange={handleUpdate}
+                    className="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
+                    placeholder="Name"
+                    type="text"
+                  />
+                </div>
+                <div className="p-2 w-1/2">
+                  <input
+                    name="email"
+                    value={form.email}
+                    onChange={handleUpdate}
+                    className="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none focus:border-indigo-500 text-base px-4 py-2"
+                    placeholder="Email"
+                    type="email"
+                  />
+                </div>
+                <div className="p-2 w-full">
+                  <textarea
+                    value={form.message}
+                    name="message"
+                    onChange={handleUpdate}
+                    className="w-full bg-gray-800 rounded border border-gray-700 text-white focus:outline-none h-48 focus:border-indigo-500 text-base px-4 py-2 resize-none block"
+                    placeholder="Message"
+                  ></textarea>
+                </div>
+                <div className="p-2 w-full">
+                  <button
+                    onClick={handleSend}
+                    disabled={buttonDisabled}
+                    className={`${
+                      buttonDisabled && "opacity-50 cursor-not-allowed"
+                    } flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg`}
+                  >
+                    Button
+                  </button>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </section>
   );
