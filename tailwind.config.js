247c5b4a00c4645160de700daefc87b0c89cb1cd
windows-user0 <windows-user0@protonmail.com>
2020-06-04 00:22:18 +0200

    Cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,13 @@
+// tailwind.config.js
 module.exports = {
-<<<<<<< HEAD
-<<<<<<< HEAD
   purge: {
-    // enabled: true,
+    enabled: true,
     content: ["./src/**/*.html", "./src/**/*.tsx"],
-=======
-  purge: [],
-  theme: {
-    extend: {},
->>>>>>> parent of a3fbdef... Cleanup 2- packages and css
-=======
-  purge: [],
-  theme: {
-    extend: {},
->>>>>>> parent of a3fbdef... Cleanup 2- packages and css
   },
+
+  theme: {},
+
   variants: {},
+
   plugins: [],
-}
+};
