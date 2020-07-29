205e4d6415e0c835c697ff4e9d58eb308620bd2d
windows-user0 <windows-user0@protonmail.com>
2020-07-29 14:01:33 +0200

    Added calendarly

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,62 @@ import * as React from "react";
 import Post from "../components/Post";
 import articles from "../data/articles";
 import ChatBubbles from "../components/ChatBubbles";
+import Loading from "../components/Loading";
+import ReactDOM from "react-dom";
+interface CalendlyProps {
+    setOpenCalendar: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Calendly = ({ setOpenCalendar }: CalendlyProps) => {
+    const [loaded, setLoaded] = React.useState(false);
+    return (
+        <div
+            className="fixed w-full h-full flex items-center text-center justify-center shadow-lg"
+            style={{
+                background: "rgba(0,0,0,0.2)",
+                zIndex: 2,
+            }}
+            onClick={() => {
+                setOpenCalendar(false);
+            }}
+        >
+            {loaded ? null : (
+                <div className="fixed">
+                    <Loading />
+                </div>
+            )}
+            <div
+                className="absolute top-0 text-3xl m-4 left-0 cursor-pointer bg-gray-200 text-gray-600 rounded-full"
+                onClick={() => {
+                    setOpenCalendar(false);
+                }}
+            >
+                X
+            </div>
+            <iframe
+                src="https://calendly.com/lets-talk-/30min"
+                className="rounded-lg modal"
+                onLoad={() => setLoaded(true)}
+            />
+        </div>
+    );
+};
 export default () => {
+    const [openCalendar, setOpenCalendar] = React.useState(false);
+    const portalDiv = document.getElementById("modal-portal");
+
+    const portalInModal = () => {
+        if (!portalDiv) return;
+        return ReactDOM.createPortal(
+            <Calendly setOpenCalendar={setOpenCalendar} />,
+            portalDiv
+        );
+    };
+
     return (
         <>
+            {" "}
+            {openCalendar && portalInModal()}
             <div className="flex flex-col pt-5 lg:pt-20 content-center items-center justify-center">
                 <div className="flex  flex-col xl:flex-row w-full xl:w-3/4 xl:justify-center  items-end md:items-end">
                     <div className="flex flex-col   w-full lg:pr-5 justify-center w-full items-center   md:text-left text-center ">
@@ -12,17 +65,12 @@ export default () => {
                             I'm <span className="text-blue-500">Alex</span>
                         </div>
                         <div className="text-4xl font-bold  text-center">
-                            And I want to be your best hire yet.
+                            And I want to be your go to developer.
                         </div>
 
                         <div className="flex flex-row pt-10 text-base whitespace-no-wrap lg:text-lg font-semibold items-center justify-center md:justify-start">
                             <div
-                                onClick={() =>
-                                    window.Calendly.initPopupWidget({
-                                        url:
-                                            "https://calendly.com/lets-talk-/30min",
-                                    })
-                                }
+                                onClick={() => setOpenCalendar(!openCalendar)}
                                 className="cursor-pointer rounded transition hover:text-blue-500 hover:bg-gray-200 bg-white ease-out duration-200 rounded-full shadow-xl active:shadow-sm hover:shadow-md text-gray-900 px-5 py-2 mr-5"
                             >
                                 Schedule a call with me
